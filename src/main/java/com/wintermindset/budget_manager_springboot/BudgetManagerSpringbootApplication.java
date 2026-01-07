5582d6b813e8f2654eb4e8fdbdc203a1b3424ed5
wintermindset <wintermindset@yandex.ru>
2026-01-07 01:39:22 -0800

    (!!!)[Other] Initial commit

---
+++ src/main/java/com/wintermindset/budget_manager_springboot/BudgetManagerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wintermindset.budget_manager_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagerSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wintermindset/budget_manager_springboot/BudgetManagerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wintermindset.budget_manager_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

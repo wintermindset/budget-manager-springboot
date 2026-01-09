d1ef5b467b66bdd06adeb33db13088673a462f82
wintermindset <wintermindset@yandex.ru>
2026-01-09 03:40:23 -0800

    (WIP)[Feature] Transaction.java was created (#1)
    Transaction.java class was created with two enums: TransactionCategory.java and TransactionType.java.

---
+++ src/main/java/com/wintermindset/budget_manager_springboot/domain/TransactionCategory.java
@@ -0,0 +1,20 @@
+package com.wintermindset.budget_manager_springboot.domain;
+
+public enum TransactionCategory {
+    // INCOME
+    SALARY,
+    BONUS,
+    INVESTMENT,
+    GIFT,
+    OTHER_INCOME,
+    
+    // EXPENSE
+    FOOD,
+    TRANSPORT,
+    ENTERTAINMENT,
+    HEALTH,
+    UTILITIES,
+    EDUCATION,
+    SHOPPING,
+    OTHER_EXPENSE
+}
